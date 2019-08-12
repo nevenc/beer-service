ba380c5eebaaa225664e3414c0e1e0db13ca2fe3
Neven Cvetkovic <nevenc@pivotal.io>
2019-08-12 11:42:58 +0200

    Initial commit.
    Empty Spring Boot project (start.spring.io) with the following starters:
    - Spring Boot Actuator
    - Rest Repositories
    - Spring Data JPA
    - Lombok
    - H2
    - MySQL

---
+++ src/main/java/io/pivotal/examples/beers/BeerServiceApplication.java
@@ -0,0 +1,13 @@
+package io.pivotal.examples.beers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeerServiceApplication.class, args);
+	}
+
+}
